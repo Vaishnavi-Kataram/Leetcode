6540585d9a0aeb9b912f1edaf44962fdf3a44ccd
VihaCodes <127044243+Vaishnavi-Kataram@users.noreply.github.com>
2026-01-23 12:12:43 -0500

    Time: 155 ms (82.02%), Space: 17 MB (41.57%) - LeetHub

---
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def reductionOperations(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort(reverse=True)
+        ans=0
+
+        for i in range(1, len(nums)):
+            if nums[i]<nums[i-1]:
+                ans+=i
+        return ans
