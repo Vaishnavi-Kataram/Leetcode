129712a702d550e1ab6e981583bab145b4ffc2c4
VihaCodes <127044243+Vaishnavi-Kataram@users.noreply.github.com>
2026-01-25 12:30:00 -0500

    Time: 1026 ms (54.11%), Space: 22 MB (17.66%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -4,5 +4,31 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        nums.sort()
-        return nums
+        # nums.sort()
+        # return nums
+
+        if len(nums)<=1:
+            return nums
+
+        mid = len(nums)//2
+        left = self.sortArray(nums[:mid])
+        right = self.sortArray(nums[mid:])
+
+        return self.merge(left, right)
+    
+    def merge(self, left, right):
+        result = []
+        i = j = 0
+
+        while i <len(left) and j < len(right):
+            if left[i] <= right[j]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+
+        result.extend(left[i:])
+        result.extend(right[j:])
+
+        return result
